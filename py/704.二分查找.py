2250627bac826e184853bce68d2f02df006518f2
wwstory <1018472917@qq.com>
2021-09-07 01:32:11 +0800

    add 704

---
+++ py/704.二分查找.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=704 lang=python3
+#
+# [704] 二分查找
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
+# @lc code=end
+
