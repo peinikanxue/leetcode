ada0d794eb6f82dc5a2f31039b48f3f08d46a82f
wwstory <1018472917@qq.com>
2021-09-24 18:36:02 +0800

    add 430 ☁

---
+++ py/430.扁平化多级双向链表.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=430 lang=python3
+#
+# [430] 扁平化多级双向链表
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Node') -> 'Node':
+        
+# @lc code=end
+
